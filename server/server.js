051b0a370d9dbc7922d4a9c2b078cebbb8eb5a37
Yarden Sibony <yardensibonyy@gmail.com>
2018-01-22 18:12:41 +0200

    Setup server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const path = require('path');
 var express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(publicPath));
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+    console.log(`Server is up on port ${port}`);
 });
