b1d2914305833cdbee888c951c3fa083af78e41a
Yarden Sibony <yardensibonyy@gmail.com>
2018-01-24 12:31:02 +0200

    Scrool to bottom if user close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
-var socket = io();
+const socket = io();
 //We are not using arrow functions since not all the browsers support them
+
+function scrollToBottom () {
+    //Selectors
+    let messages = jQuery('#messages');
+    let newMessage = messages.children('li:last-child');
+    //Heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -18,6 +35,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', (message) => {
@@ -30,7 +48,7 @@ socket.on('newLocationMessage', (message) => {
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
     // let li = jQuery('<li></li>');
     // let a = jQuery('<a target="_blank">My current location</a>'); //target="_blank" opens link in a new tab
     // li.text(`${message.from} ${formattedMoment}: `);
